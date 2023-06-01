a8b57223b60f0ef7129925fb518ef6a28622fb24
wings denzel <1248482491.com>
2023-06-01 18:37:14 +0800

    opt

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="d-flex align-items-center justify-content-sm-center" style="height: 80px; padding: 10px 5vw">
+  <div
+    class="d-flex align-items-center justify-content-center justify-content-md-center"
+    style="height: 80px; padding: 10px 5vw"
+  >
     <img src="/img/logo.svg" alt="" style="height: 60px" />
-    <img src="/img/title.svg" alt="中国汉服文化网" style="height: 40px; margin-left: 12px" />
+    <img
+      src="/img/title.svg"
+      alt="中国汉服文化网"
+      style="height: 40px; margin-left: 12px"
+    />
     <div class="ms-auto d-flex align-items-center pc-nav">
-      <a class="nav-link" href="/"> 首页 </a>
+      <a class="nav-link" href="/">首页</a>
       <div class="nav-divider"></div>
       <a class="nav-link" href="/news">汉服资讯</a>
       <div class="nav-divider"></div>
@@ -27,99 +34,100 @@
 </template>
 
 <style lang="less" scoped>
-  .nav-link {
-    padding: 0 12px;
-    font-size: 24px;
-    font-family: calligraphy;
+.nav-link {
+  padding: 0 12px;
+  font-size: 24px;
+  font-family: calligraphy;
+  cursor: pointer;
+}
+
+.nav-divider {
+  width: 2px;
+  height: 20px;
+  background: #000;
+}
+
+.menuToggle {
+  position: relative;
+  width: 33px;
+  height: 23px;
+  cursor: pointer;
+  input {
+    display: block;
+    width: 100%;
+    height: 100%;
+    position: absolute;
     cursor: pointer;
+    opacity: 0; /* hide this */
+    z-index: 2; /* and place it over the hamburger */
+    -webkit-touch-callout: none;
   }
 
-  .nav-divider {
-    width: 2px;
-    height: 20px;
-    background: #000;
-  }
-
-  .menuToggle {
-    position: relative;
+  span {
+    display: block;
     width: 33px;
-    height: 23px;
-    cursor: pointer;
-    input {
-      display: block;
-      width: 100%;
-      height: 100%;
-      position: absolute;
-      cursor: pointer;
-      opacity: 0; /* hide this */
-      z-index: 2; /* and place it over the hamburger */
-      -webkit-touch-callout: none;
-    }
-
-    span {
-      display: block;
-      width: 33px;
-      height: 4px;
-      margin-bottom: 5px;
-      position: relative;
-      background: #000;
-      border-radius: 3px;
-      z-index: 1;
-      transform-origin: 4px 0px;
-      transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1), background 0.5s cubic-bezier(0.77, 0.2, 0.05, 1), opacity 0.55s ease;
-      &:first-child {
-        transform-origin: 0% 0%;
-      }
-
-      &:nth-last-child(2) {
-        transform-origin: 0% 100%;
-      }
-    }
-
-    input:checked ~ span {
-      opacity: 1;
-      transform: rotate(45deg) translate(-2px, -1px);
-      background: #232323;
-    }
-    input:checked ~ span:nth-last-child(3) {
-      opacity: 0;
-      transform: rotate(0deg) scale(0.2, 0.2);
-    }
-    input:checked ~ span:nth-last-child(2) {
-      transform: rotate(-45deg) translate(0, -1px);
-    }
-
-    input:checked ~ .menu {
-      transform: none;
-    }
-
-    .menu {
-      position: fixed;
-      top: 0;
-      bottom: 0;
-      left: 0;
-      right: 0;
-      background: #ededed;
-      list-style-type: none;
-      -webkit-font-smoothing: antialiased;
-      /* to stop flickering of text in safari */
-
+    height: 4px;
+    margin-bottom: 5px;
+    position: relative;
+    background: #000;
+    border-radius: 3px;
+    z-index: 1;
+    transform-origin: 4px 0px;
+    transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1),
+      background 0.5s cubic-bezier(0.77, 0.2, 0.05, 1), opacity 0.55s ease;
+    &:first-child {
       transform-origin: 0% 0%;
-      transform: translate(100%, 0);
+    }
 
-      transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1);
+    &:nth-last-child(2) {
+      transform-origin: 0% 100%;
     }
   }
 
-  @media screen and (max-width: 860px) {
-    .pc-nav {
-      display: none !important;
-    }
+  input:checked ~ span {
+    opacity: 1;
+    transform: rotate(45deg) translate(-2px, -1px);
+    background: #232323;
+  }
+  input:checked ~ span:nth-last-child(3) {
+    opacity: 0;
+    transform: rotate(0deg) scale(0.2, 0.2);
+  }
+  input:checked ~ span:nth-last-child(2) {
+    transform: rotate(-45deg) translate(0, -1px);
   }
 
-  @media screen and (min-width: 860px) {
-    .mobile-nav {
-      display: none !important;
-    }
+  input:checked ~ .menu {
+    transform: none;
   }
+
+  .menu {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: #ededed;
+    list-style-type: none;
+    -webkit-font-smoothing: antialiased;
+    /* to stop flickering of text in safari */
+
+    transform-origin: 0% 0%;
+    transform: translate(100%, 0);
+
+    transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1);
+  }
+}
+
+@media screen and (max-width: 860px) {
+  .pc-nav {
+    display: none !important;
+  }
+}
+
+@media screen and (min-width: 860px) {
+  .mobile-nav {
+    display: none !important;
+  }
+}
 </style>
--- pages/news.vue
+++ pages/news.vue
@@ -3,24 +3,63 @@
     <div class="response-content">
       <div class="row">
         <div class="col-12 col-md-6 mt-3 lv-1">
-          <div id="banner" class="carousel slide overflow-hidden" style="border-radius: 12px">
+          <div
+            id="banner"
+            class="carousel slide overflow-hidden"
+            style="border-radius: 12px"
+          >
             <div class="carousel-indicators">
-              <button type="button" data-bs-target="#banner" v-for="(b, index) in banner" :data-bs-slide-to="index" :class="[index === 0 && 'active']" :key="index"></button>
+              <button
+                type="button"
+                data-bs-target="#banner"
+                v-for="(b, index) in banner"
+                :data-bs-slide-to="index"
+                :class="[index === 0 && 'active']"
+                :key="index"
+              ></button>
             </div>
             <div class="carousel-inner">
-              <div :class="['carousel-item', index === 0 && 'active']" v-for="(b, index) in banner" :key="index">
-                <img :src="b.url" class="ss-img d-block w-100" style="object-fit: cover" alt="" />
-                <span class="fs-6 fs-md-5 text-light f-han" style="position: absolute; bottom: 0; left: 1rem; z-index: 2">
+              <div
+                :class="['carousel-item', index === 0 && 'active']"
+                v-for="(b, index) in banner"
+                :key="index"
+              >
+                <img
+                  :src="b.url"
+                  class="ss-img d-block w-100"
+                  style="object-fit: cover"
+                  alt=""
+                />
+                <span
+                  class="fs-6 fs-md-5 text-light f-han"
+                  style="position: absolute; bottom: 0; left: 1rem; z-index: 2"
+                >
                   {{ b.title }}
                 </span>
               </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#banner" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <button
+              class="carousel-control-prev"
+              type="button"
+              data-bs-target="#banner"
+              data-bs-slide="prev"
+            >
+              <span
+                class="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
               <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#banner" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <button
+              class="carousel-control-next"
+              type="button"
+              data-bs-target="#banner"
+              data-bs-slide="next"
+            >
+              <span
+                class="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
               <span class="visually-hidden">Next</span>
             </button>
           </div>
@@ -34,7 +73,12 @@
           </div>
 
           <div class="hot-news fs-6 f-han">
-            <div v-for="(_, index) in new Array(6).fill()" :key="index" @mouseover="active = index" :class="['li-item', active === index && 'active']">
+            <div
+              v-for="(_, index) in new Array(6).fill()"
+              :key="index"
+              @mouseover="active = index"
+              :class="['li-item', active === index && 'active']"
+            >
               <div class="image-box">
                 <img :src="`/img/banner/banner${index}.jpg`" alt="" />
               </div>
@@ -46,16 +90,28 @@
       </div>
 
       <div class="row mt-3">
-        <div class="deco-title d-flex align-items-end">
+        <div class="deco-title d-flex align-items-end mb-3">
           <img src="/img/brand.png" style="height: 40px" alt="" />
           <div class="fs-3 f-han ms-3">要点资讯</div>
           <div class="fs-5 ms-2">Key News</div>
         </div>
-        <div class="col-12 col-md-4 mt-3" v-for="(_, i) in 12" :key="i">
+        <div class="col-12 col-md-4 mb-3" v-for="(_, i) in 12" :key="i">
           <div class="card shadow">
             <img class="left-img shadow" src="/img/banner/banner2.jpg" alt="" />
-            <div class="d-flex flex-column justify-content-center f-han fs-6 fs-md-5 ms-4 p-4">
-              <div>汉服产业春风得意，总规模超十亿 周边产业多点开花</div>
+            <div
+              class="d-flex flex-column justify-content-center f-han fs-6 fs-md-5 p-3"
+            >
+              <div
+                style="
+                  display: -webkit-box;
+                  -webkit-line-clamp: 3;
+                  -webkit-box-orient: vertical;
+                  overflow: hidden;
+                  line-height: 1.1;
+                "
+              >
+                汉服产业春风得意，总规模超十亿 周边产业多点开花
+              </div>
               <div class="f-han fs-6 fs-md-5 ms-auto mt-auto">点击浏览</div>
             </div>
           </div>
@@ -65,149 +121,149 @@
   </div>
 </template>
 <script setup>
-  definePageMeta({
-    layout: "h-c-f",
-  });
+definePageMeta({
+  layout: "h-c-f",
+});
 
-  const banner = reactive([
-    { url: "/img/banner/banner0.jpg", title: "少年志｜薪火相传" },
-    { url: "/img/banner/banner1.jpg", title: "少年志｜薪火相传" },
-    { url: "/img/banner/banner2.jpg", title: "少年志｜薪火相传" },
-    { url: "/img/banner/banner3.jpg", title: "少年志｜薪火相传" },
-    { url: "/img/banner/banner4.jpg", title: "少年志｜薪火相传" },
-  ]);
+const banner = reactive([
+  { url: "/img/banner/banner0.jpg", title: "少年志｜薪火相传" },
+  { url: "/img/banner/banner1.jpg", title: "少年志｜薪火相传" },
+  { url: "/img/banner/banner2.jpg", title: "少年志｜薪火相传" },
+  { url: "/img/banner/banner3.jpg", title: "少年志｜薪火相传" },
+  { url: "/img/banner/banner4.jpg", title: "少年志｜薪火相传" },
+]);
 
-  const active = ref(0);
+const active = ref(0);
 </script>
 <style lang="less" scoped>
-  .hot-news {
-    * {
-      transition: all 0.5s;
+.hot-news {
+  * {
+    transition: all 0.5s;
+  }
+  .active {
+    &.li-item {
     }
-    .active {
-      &.li-item {
-      }
-      .image-box {
-        height: 100% !important;
-        &::after {
-          opacity: 1 !important;
-        }
-      }
-
-      .number {
-        z-index: 1;
-        color: #fff;
-      }
-      .text {
-        z-index: 1;
-        color: #fff;
+    .image-box {
+      height: 100% !important;
+      &::after {
+        opacity: 1 !important;
       }
     }
 
-    .li-item {
+    .number {
+      z-index: 1;
+      color: #fff;
+    }
+    .text {
+      z-index: 1;
+      color: #fff;
+    }
+  }
+
+  .li-item {
+    position: relative;
+    height: 1.5rem;
+    .image-box {
+      height: 0;
+      border-radius: 12px;
+      overflow: hidden;
+      width: 100%;
       position: relative;
-      height: 1.5rem;
-      .image-box {
-        height: 0;
-        border-radius: 12px;
-        overflow: hidden;
+      img {
         width: 100%;
-        position: relative;
-        img {
-          width: 100%;
-          height: 100%;
-          object-fit: cover;
-        }
-        &::after {
-          content: "";
-          position: absolute;
-          opacity: 0;
-          bottom: 0;
-          left: 0;
-          right: 0;
-          height: 2rem;
-          background: linear-gradient(180deg, transparent, rgba(0, 0, 0, 0.493));
-          border-radius: 0 0 12px 12px;
-        }
+        height: 100%;
+        object-fit: cover;
       }
-      .number {
-        margin-left: 0.5rem;
+      &::after {
+        content: "";
         position: absolute;
+        opacity: 0;
         bottom: 0;
         left: 0;
-      }
-      .text {
-        margin-left: 1.5rem;
-        position: absolute;
-        bottom: 0;
-        left: 0;
+        right: 0;
+        height: 2rem;
+        background: linear-gradient(180deg, transparent, rgba(0, 0, 0, 0.493));
+        border-radius: 0 0 12px 12px;
       }
     }
-    .li-item:nth-of-type(1) {
-      .number {
-        color: rgb(255, 0, 0);
-      }
+    .number {
+      margin-left: 0.5rem;
+      position: absolute;
+      bottom: 0;
+      left: 0;
     }
+    .text {
+      margin-left: 1.5rem;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+    }
+  }
+  .li-item:nth-of-type(1) {
+    .number {
+      color: rgb(255, 0, 0);
+    }
+  }
 
-    .li-item:nth-of-type(2) {
-      .number {
-        color: rgb(255, 85, 0);
-      }
+  .li-item:nth-of-type(2) {
+    .number {
+      color: rgb(255, 85, 0);
     }
+  }
 
-    .li-item:nth-of-type(3) {
-      .number {
-        color: rgb(255, 153, 0);
-      }
+  .li-item:nth-of-type(3) {
+    .number {
+      color: rgb(255, 153, 0);
+    }
+  }
+}
+
+.card {
+  display: flex;
+  flex-direction: row;
+  border-radius: 12px;
+  border: none;
+  .left-img {
+    border-radius: 12px;
+    width: 60%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+
+@media (max-width: 1200px) {
+  .lv-1 {
+    height: 300px;
+    .ss-img {
+      height: 300px;
+    }
+  }
+  .active {
+    &.li-item {
+      height: 142px !important;
+    }
+  }
+  .card {
+    height: 100px;
+  }
+}
+
+@media (min-width: 1200px) {
+  .lv-1 {
+    height: 500px;
+    .ss-img {
+      height: 500px;
+    }
+  }
+
+  .active {
+    &.li-item {
+      height: 343px !important;
     }
   }
 
   .card {
-    display: flex;
-    flex-direction: row;
-    border-radius: 12px;
-    border: none;
-    .left-img {
-      border-radius: 12px;
-      width: 60%;
-      height: 100%;
-      object-fit: cover;
-    }
+    height: 200px;
   }
-
-  @media (max-width: 1200px) {
-    .lv-1 {
-      height: 300px;
-      .ss-img {
-        height: 300px;
-      }
-    }
-    .active {
-      &.li-item {
-        height: 142px !important;
-      }
-    }
-    .card {
-      height: 100px;
-    }
-  }
-
-  @media (min-width: 1200px) {
-    .lv-1 {
-      height: 500px;
-      .ss-img {
-        height: 500px;
-      }
-    }
-
-    .active {
-      &.li-item {
-        height: 343px !important;
-      }
-    }
-
-    .card {
-      height: 200px;
-    }
-  }
+}
 </style>
